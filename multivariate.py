51b7026b476c3525c4d6534b8351e824cabf0d35
zhenyuen <zhenyuen.dev@gmail.com>
2023-03-01 21:47:21 +0000

    feat: define steepest descent method and conjugate gradient method

--- multivariate.py
+++ multivariate.py
@@ -3,19 +3,28 @@ import numpy as np
 def newton_rhapson_univariate(f, df, ddf, start, precision, max_iter):
     x = start
     for i in range(max_iter):
-        d = - df(x) / ddf(x)
+        d = -df(x) / ddf(x)
 
         if (abs(d) < precision): break
         
         x += d 
+    print(f"Iterations: {i}")
     return x
 
 
-def steepest_descent():
-    pass
+def steepest_descent(f, df, ddf, start, precision, max_iter):
+    x = start
+    for i in range(max_iter):
+        d = -df(x)
+        lr = d.T @ d / (d.T @ ddf(x) @ d)
+        delta = lr * d
+        if np.all(np.abs(delta) < precision): break
+        x += delta
+    print(f"Iterations: {i}")
+    return x
 
 
-def newton_rhapson_multivariate():
+def newton_rhapson_multivariate(f, df, ddf, start, precision, max_iter):
     pass
 
 
@@ -23,21 +32,52 @@ def barzilai_borwein():
     pass
 
 
-def conjugate_gradient():
-    pass
+def conjugate_gradient(f, df, ddf, start, precision, max_iter):
+    x = start
+    d = -df(x)
+    lr = d.T @ d / (d.T @ ddf(x) @ d)
+    delta = lr * d
+    
+
+    for i in range(max_iter):
+        x += delta
+
+        dfx = df(x)
+        beta = (dfx.T @ dfx) / (d.T @ d)
+
+        d = -dfx + beta * d
+        lr = -(d.T @ dfx) / (d.T @ ddf(x) @ d)
+
+        delta = lr * d
+        if np.all(np.abs(delta) < precision): break
+
+    print(f"Iterations: {i}")
+    return x
 
 
 def newton_gauss():
     pass
 
+# Manual unit testing, t
+# f = lambda x: 1 / ((x ** 0.148) * (3 * np.pi - 16 * x))
+# df = lambda x: -0.148 * (x ** -1.148) / (3 * np.pi - 16 * x) + 16 * x ** -0.148 / (3 * np.pi - 16 * x) ** 2
+# ddf = lambda x: 0.170 * (x ** -2.148) / (3 * np.pi - 16 * x) - 4.736 * x ** -1.148 / (3 * np.pi - 16 * x) ** 2 + 512 * x ** -0.148 * (3 * np.pi - 16 * x) ** -3
 
-f = lambda x: 1 / ((x ** 0.148) * (3 * np.pi - 16 * x))
-df = lambda x: -0.148 * (x ** -1.148) / (3 * np.pi - 16 * x) + 16 * x ** -0.148 / (3 * np.pi - 16 * x) ** 2
-ddf = lambda x: 0.170 * (x ** -2.148) / (3 * np.pi - 16 * x) - 4.736 * x ** -1.148 / (3 * np.pi - 16 * x) ** 2 + 512 * x ** -0.148 * (3 * np.pi - 16 * x) ** -3
+# start = (f(0.15) + f(0.1)) / 2
+# precision = 1E-10
+# max_iter = 1000
 
-start = (f(0.15) + f(0.1)) / 2
-precision = 10E-6
+# print(newton_rhapson_univariate(f, df, ddf, start, precision, max_iter))
+# print(f(0.07576556030681753), f(0.07593962581838594))
+
+
+f = lambda x: x.T @ np.array([[25, 0], [0, 20]]) @ x + np.array([[-1, -1]]) @ x
+df = lambda x: np.array([[50, 0], [0, 40]]) @ x - np.array([[-1, -1]]).T
+ddf = lambda x: np.array([[50, 0], [0, 40]])
+#print(f(np.array([[0.02, 0.025]]).T))
+start = np.array([[3., 1.]]).T
+precision = 1E-10
 max_iter = 1000
-
-print(newton_rhapson_univariate(f, df, ddf, start, precision, max_iter))
-print(f(0.07576556030681753), f(0.07593962581838594))
+# print(steepest_descent(f, df, ddf, start, precision, max_iter))
+# print(np.matmul(np.array([[1, 2]]).T, np.array([[1, 2]])))
+print(conjugate_gradient(f, df, ddf, start, precision, max_iter))
